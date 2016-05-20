52c8b19908058dca4dd6b49373ccde704e73935e
brintsev.aleksandr <brintsev.aleksandr@gmail.com>
2016-05-20 16:30:22 +0300

    New post form added

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
+from django.shortcuts import redirect
+from django.contrib.auth.models import User
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -9,3 +12,20 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post':post})
+    
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = User.objects.get(username='admin')
+            #post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            
+            posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+            return render(request, 'blog/post_list.html', {'posts': posts})
+    else:
+        form = PostForm()
+    
+    return render(request, 'blog/post_edit.html', {'form': form})
